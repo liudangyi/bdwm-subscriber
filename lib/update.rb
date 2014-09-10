897dfdcd6bcb11a9bfc97b8ff6d490383426d6bd
刘当一 <LeedyPKU@gmail.com>
2014-09-10 17:05:11 +0800

    replace smtp config

--- lib/update.rb
+++ lib/update.rb
@@ -40,7 +40,7 @@ def send_all_mail
     next if contents.empty?
     body = ERB.new(File.read("views/email.erb")).result(binding)
     Pony.mail to: email,
-              from: "bdwm.subscriper@leedy.me",
+              from: CONFIG["smtp"]["user_name"],
               subject: "[BDWM 订阅] #{contents.map(&:first).uniq.join(", ")} 版面提醒",
               charset: "UTF-8",
               html_body: body,
